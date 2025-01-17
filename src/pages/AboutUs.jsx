1d552e8b25abb5dd6ea8f7b7ca9b234015cffd90
timepassuser <159204696+timepassuser@users.noreply.github.com>
2025-01-18 03:53:03 +0530

    Added background image and Casino.ttf font

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import styles from '../styles/AboutUs.module.css'
 
+function RewardsSection() {
+  return <>
+    <h2 className={styles.subheading + " " + styles.casinoFont}> REWARDS</h2>
+  </>
+}
+
+function BlackTint() {
+  return <span className={styles.blacktint}></span>
+}
+
 const AboutUs = () => {
   return (
     <>
-        <div>
-            <h1 className={styles.about}>About Us Page</h1>
-        </div>
+          <div className={styles.backgroundImage}/>
+          <BlackTint/>
+          <RewardsSection/>
     </>
   )
 }
