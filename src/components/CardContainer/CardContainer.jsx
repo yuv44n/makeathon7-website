802f421dc801654808f0dde8fe01364bd5a7dc6a
Whacky Ghost <nischeysingh@gmail.com>
2025-01-19 22:09:11 +0530

    fixed card spread and viewport climing issue

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -10,10 +10,10 @@ import tracksImage from "../../assets/tracks.png";
 
 const CardContainer = () => {
   const cards = [
-    { angle: "-30deg", xOffset: "-15vw", backgroundImage: aboutImage, link: "/about" },
-    { angle: "-10deg", xOffset: "-5vw", backgroundImage: sponsorsImage, link: "/sponsors" },
-    { angle: "10deg", xOffset: "5vw", backgroundImage: timelineImage, link: "/timeline" },
-    { angle: "30deg", xOffset: "15vw", backgroundImage: tracksImage, link: "/tracks" },
+    { angle: "-15deg", xOffset: "-10vw", backgroundImage: aboutImage, link: "/about" },
+    { angle: "-5deg", xOffset: "-2.5vw", backgroundImage: sponsorsImage, link: "/sponsors" },
+    { angle: "5deg", xOffset: "2.5vw", backgroundImage: timelineImage, link: "/timeline" },
+    { angle: "15deg", xOffset: "10vw", backgroundImage: tracksImage, link: "/tracks" },
   ];
 
   return (
