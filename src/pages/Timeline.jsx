ea01118614b5e27cb9f0adabc6f2ae4cc22c1e89
Harkirat Singh <singharkirath1511@gmail.com>
2025-01-20 21:28:10 +0530

    commit1

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from '../styles/Timeline.module.css'
 
+
 const Timeline = () => {
   return (
     <>
-        <div>
-            <h1 className={styles.timeline}>Timeline Page</h1>
+        <div className='h-screen w-full' >
+            <h1 className={styles.timeline}>Timeline Page is very fuddu</h1>
+            <img src="bgimg.jpg" alt="Background" />
         </div>
     </>
   )
