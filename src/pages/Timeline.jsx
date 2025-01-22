57650b0e39f2aacff15a799602a95eef29fb039e
Kevin-M-scs <ktmcolumban@gmail.com>
2025-01-22 22:29:03 +0530

    event card

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import styles from '../styles/Timeline.module.css'
-import eventcard from '../components/images/image1.png'
+
 
 const Timeline = () => {
   return (
     <>
         <div className={styles.background}>
-            <div className={styles.timeline}>Timeline Page</div>
+            <div className={styles.timeline}>
+              <p>EVENT: EVENT 1</p>
+              <br/>
+              <p>DATE: 11/11/24</p>
+              <p>TIME: 10:00 am</p>
+              <p>VENUE: TAN AUDI</p>
+              <br />
+              <p className={styles.description}>This is the description for the <br/>event. It will be a great <br/>experience. We look forward <br/> to max participation.</p>
+              
+            </div>
+            
         </div>
     </>
   )
