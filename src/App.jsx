a1ec42859a83b0321da420676a6d45458a9cdc05
arnav1296 <arnavsh92@gmail.com>
2025-04-19 11:46:10 +0530

    Added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
-import Timeline from './pages/Timeline';
-import AboutUs from './pages/AboutUs';
-import Tracks from './pages/Tracks';
-import Sponsors from './pages/Sponsors';
-import AppPolicy from './pages/AppPolicy';
+import { useState, useEffect } from "react";
+import Loading from "./components/Loading";
+import Home from "./pages/Home";
+import Timeline from "./pages/Timeline";
+import AboutUs from "./pages/AboutUs";
+import Tracks from "./pages/Tracks";
+import Sponsors from "./pages/Sponsors";
+import AppPolicy from "./pages/AppPolicy";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2000);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/timeline" element={<Timeline />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/tracks" element={<Tracks />} />
-          <Route path="/sponsors" element={<Sponsors />} />
-          <Route path="/policy" element={<AppPolicy />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/tracks" element={<Tracks />} />
+        <Route path="/sponsors" element={<Sponsors />} />
+        <Route path="/policy" element={<AppPolicy />} />
+      </Routes>
     </Router>
   );
 }
